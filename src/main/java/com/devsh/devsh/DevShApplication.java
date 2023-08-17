9a2793083899a277c74d88cacb9d7f38a854920f
taohansens <taohansens@gmail.com>
2023-08-17 15:07:00 -0300

    Init Project

---
+++ src/main/java/com/devsh/devsh/DevShApplication.java
@@ -0,0 +1,13 @@
+package com.devsh.devsh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevShApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DevShApplication.class, args);
+    }
+
+}
